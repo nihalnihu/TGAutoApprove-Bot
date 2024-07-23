1d4b10d3feb458a0c3553bfd8ac984780204020c
nihalnihu <70343963+nihalnihu@users.noreply.github.com>
2024-07-23 06:49:54 +0530

    Update database.py

--- database.py
+++ database.py
@@ -30,18 +30,26 @@ def remove_user(user_id):
         return 
     return users.delete_one({"user_id": str(user_id)})
     
-def add_group(chat_id):
+def add_group(chat_id, username=None, name=None):
     in_db = already_dbg(chat_id)
     if in_db:
         return
-    return groups.insert_one({"chat_id": str(chat_id)})
-
+    group_data = {"chat_id": str(chat_id)}
+    if username:
+        group_data["username"] = username
+    if name:
+        group_data["name"] = name
+    return groups.insert_one(group_data)
+        
 def all_users():
     user = users.find({})
     usrs = len(list(user))
     return usrs
 
 def all_groups():
-    group = groups.find({})
-    grps = len(list(group))
-    return grps
+    group_cursor = groups.find({})
+    groups_list = list(group_cursor)
+    group_count = len(groups_list)
+    usernames = [group.get("username", "No Username") for group in groups_list]
+    names = [group.get("name", "No Name") for group in groups_list]
+    return group_count, usernames, names
